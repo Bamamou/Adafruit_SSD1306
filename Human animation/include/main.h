c7cff28bb63c62ec8c5a25818e683b791d261703
bamamounicolas <bamamounicolas@gmail.com>
2023-09-16 02:21:59 +0800

    First commit

---
+++ Human animation/include/main.h
@@ -0,0 +1,13 @@
+/* 
+This is the header of the main.cpp file it should call all the headers used in this project
+
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <Arduino.h>
+#include "globalConst.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#endif
